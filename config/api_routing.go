efc2cab4b08fb9797a1575496de113fd71bf46c0
Aakash-Pandit <aakashpandit366@gmail.com>
2021-10-23 16:52:34 +0530

    adds patients

--- config/api_routing.go
+++ config/api_routing.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"myapp/patients"
 	"myapp/userapp"
 
 	"github.com/gorilla/mux"
@@ -8,10 +9,17 @@ import (
 
 func UserApiRouting(router *mux.Router) {
 	userapp.CreateDummyUsers()
+	patients.CreateDummyPatients()
 
 	router.HandleFunc("/api/v1/users", userapp.GetAllUsers).Methods("GET")
 	router.HandleFunc("/api/v1/users/{id}", userapp.GetUser).Methods("GET")
 	router.HandleFunc("/api/v1/users/", userapp.CreateUser).Methods("POST")
 	router.HandleFunc("/api/v1/users/{id}", userapp.UpdateUser).Methods("PATCH")
 	router.HandleFunc("/api/v1/users/{id}", userapp.DeleteUser).Methods("DELETE")
+
+	router.HandleFunc("/api/v1/patients", patients.GetAllPatients).Methods("GET")
+	router.HandleFunc("/api/v1/patients/{id}", patients.GetPatient).Methods("GET")
+	router.HandleFunc("/api/v1/patients/", patients.CreatePatient).Methods("POST")
+	router.HandleFunc("/api/v1/patients/{id}", patients.UpdatePatient).Methods("PATCH")
+	router.HandleFunc("/api/v1/patients/{id}", patients.DeletePatient).Methods("DELETE")
 }
