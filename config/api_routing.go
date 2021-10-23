90eb52f7f2c60a94cc8cad451a2d6f771221a2f1
Aakash-Pandit <aakashpandit366@gmail.com>
2021-10-23 16:59:35 +0530

    rename package name

--- config/api_routing.go
+++ config/api_routing.go
@@ -2,20 +2,20 @@ package config
 
 import (
 	"myapp/patients"
-	"myapp/userapp"
+	"myapp/users"
 
 	"github.com/gorilla/mux"
 )
 
 func UserApiRouting(router *mux.Router) {
-	userapp.CreateDummyUsers()
+	users.CreateDummyUsers()
 	patients.CreateDummyPatients()
 
-	router.HandleFunc("/api/v1/users", userapp.GetAllUsers).Methods("GET")
-	router.HandleFunc("/api/v1/users/{id}", userapp.GetUser).Methods("GET")
-	router.HandleFunc("/api/v1/users/", userapp.CreateUser).Methods("POST")
-	router.HandleFunc("/api/v1/users/{id}", userapp.UpdateUser).Methods("PATCH")
-	router.HandleFunc("/api/v1/users/{id}", userapp.DeleteUser).Methods("DELETE")
+	router.HandleFunc("/api/v1/users", users.GetAllUsers).Methods("GET")
+	router.HandleFunc("/api/v1/users/{id}", users.GetUser).Methods("GET")
+	router.HandleFunc("/api/v1/users/", users.CreateUser).Methods("POST")
+	router.HandleFunc("/api/v1/users/{id}", users.UpdateUser).Methods("PATCH")
+	router.HandleFunc("/api/v1/users/{id}", users.DeleteUser).Methods("DELETE")
 
 	router.HandleFunc("/api/v1/patients", patients.GetAllPatients).Methods("GET")
 	router.HandleFunc("/api/v1/patients/{id}", patients.GetPatient).Methods("GET")
