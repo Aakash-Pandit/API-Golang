55a5cd0bb3d49abf229cc14c4a507a24852888a5
Aakash-Pandit <aakashpandit366@gmail.com>
2021-10-29 20:15:59 +0530

    using uuid in PK

--- organization/models.go
+++ organization/models.go
@@ -8,7 +8,7 @@ import (
 )
 
 type Organization struct {
-	ID       string    `json:"id" gorm:"primaryKey;unique;"`
+	ID       uuid.UUID `json:"id" gorm:"primaryKey;unique;"`
 	Name     string    `json:"name"`
 	Address  string    `json:"address"`
 	Created  time.Time `json:"created"`
@@ -16,6 +16,6 @@ type Organization struct {
 }
 
 func (org *Organization) BeforeCreate(scope *gorm.Scope) error {
-	(*org).ID = uuid.New().String()
+	(*org).ID = uuid.New()
 	return nil
 }
