9d8c7e25084aa2fc08b82e34b436de4130744488
Aakash-Pandit <aakashpandit366@gmail.com>
2021-11-01 18:06:53 +0530

    adds slice validation

--- organization/validations.go
+++ organization/validations.go
@@ -1,10 +1,30 @@
 package organization
 
-import "errors"
+// func (org *Organization) Validate() error {
+// 	if (*org).Name == "" {
+// 		return errors.New("name of organization can not be blank")
+// 	}
 
-func (org *Organization) Validate() error {
+// 	return nil
+// }
+
+func (org *Organization) Validate() []map[string]string {
+	var err map[string]string
+	var ValidationErrors []map[string]string
+
 	if (*org).Name == "" {
-		return errors.New("name of organization can not be blank")
+		err = map[string]string{"firstname": "name of organization can not be blank"}
+		ValidationErrors = append(ValidationErrors, err)
+	}
+
+	if (*org).Address == "" {
+		err = map[string]string{"lastname": "laname of organization can not be blank"}
+		ValidationErrors = append(ValidationErrors, err)
+
+	}
+
+	if len(ValidationErrors) > 0 {
+		return ValidationErrors
 	}
 
 	return nil
--- organization/views.go
+++ organization/views.go
@@ -57,13 +57,21 @@ func CreateOrganization(response http.ResponseWriter, request *http.Request) {
 	}
 	defer db.Close()
 
-	err = organization.Validate()
-	if err != nil {
+	// err = organization.Validate()
+	// if err != nil {
+	// 	response.WriteHeader(http.StatusBadRequest)
+	// 	detail := map[string]string{
+	// 		"details": err.Error(),
+	// 	}
+	// 	json.NewEncoder(response).Encode(detail)
+	// 	return
+	// }
+
+	// var validation_error []map[string]string
+	validation_error := organization.Validate()
+	if validation_error != nil {
 		response.WriteHeader(http.StatusBadRequest)
-		detail := map[string]string{
-			"details": err.Error(),
-		}
-		json.NewEncoder(response).Encode(detail)
+		json.NewEncoder(response).Encode(validation_error)
 		return
 	}
 
