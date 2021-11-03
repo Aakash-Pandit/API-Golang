778046a44f05483f969b6a82bea65d344ab49f78
Aakash-Pandit <aakashpandit366@gmail.com>
2021-11-03 18:19:58 +0530

    update token time

--- core/authentication.go
+++ core/authentication.go
@@ -16,7 +16,7 @@ func CreateToken(userid uuid.UUID) (string, error) {
 	atClaims := jwt.MapClaims{}
 	atClaims["authorized"] = true
 	atClaims["user_id"] = userid
-	atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix()
+	atClaims["exp"] = time.Now().Add(time.Minute * 30).Unix()
 	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
 	token, err := at.SignedString([]byte(GoDotEnvVariable("ACCESS_SECRET")))
 
